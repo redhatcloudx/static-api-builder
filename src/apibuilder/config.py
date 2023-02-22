8f3692e769724eab9fffce610f62645890cf46d2
Major Hayden <major@mhtx.net>
2023-02-22 10:36:38 -0600

    Add initial code

---
+++ src/apibuilder/config.py
@@ -0,0 +1,5 @@
+"""Configuration variables."""
+
+AWS_RAW_DATA = "https://poc.imagedirectory.cloud/raw/aws/index.json.zst"
+AZURE_RAW_DATA = "https://poc.imagedirectory.cloud/raw/azure/index.json.zst"
+GCP_RAW_DATA = "https://poc.imagedirectory.cloud/raw/gcp/index.json.zst"
