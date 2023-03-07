d36cefc621b0d78a011d6a721d8f606aa2d6bf97
Major Hayden <major@mhtx.net>
2023-03-07 15:59:55 -0600

    Write based on AMI ID

--- generate_aws_idx.py
+++ generate_aws_idx.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 AWS_JSON_DIR = "cloud-json/aws"
 AWS_JSON_GLOB = f"{AWS_JSON_DIR}/*.json"
-AWS_IDX_DIR = "public/idx/aws/ownerid"
+AWS_IDX_DIR = "public/idx/aws"
 
 
 def get_regions() -> list:
@@ -48,6 +48,7 @@ def read_json(region: str) -> pd.DataFrame:
 
 def write_owner_ids(df):
     """Split the data frames by OwnerId."""
+    print("Writing images based on OwnerId")
     for owner_id in df.OwnerId.unique():
         owner_df = df[df.OwnerId == owner_id]
 
@@ -56,16 +57,30 @@ def write_owner_ids(df):
             continue
 
         print(f"Writing owner {owner_id} to file")
-        output_dir = f"{AWS_IDX_DIR}/{owner_id}"
+        output_dir = f"{AWS_IDX_DIR}/ownerid/{owner_id}"
         makedirs(output_dir)
-        owner_df.to_json(f"{output_dir}/index.html", orient="records")
+        owner_df.to_json(f"{output_dir}/index.json", orient="records")
+
+
+def write_image_ids(df):
+    """Split the data frames by ImageID."""
+    print("Writing images based on ImageId")
+    for image_id in df.ImageId.unique():
+        image_df = df[df.ImageId == image_id]
+
+        output_dir = f"{AWS_IDX_DIR}/imageid/{image_id}"
+        makedirs(output_dir)
+        image_df.to_json(f"{output_dir}/index.json", orient="records")
 
 
 def main():
     """Main function."""
     df = read_data()
     df.info()
+    print(f"Total rows: {len(df.index)}")
+    print(f"Unique ImageID: {len(df.ImageId.unique())}")
     write_owner_ids(df)
+    write_image_ids(df)
 
 
 if __name__ == "__main__":
